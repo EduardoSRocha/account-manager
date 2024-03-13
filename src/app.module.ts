53bad99230e0a2115d2a905d2e3f9be7e1fce07c
EddieUFSM <eddie.maneki@gmail.com>
2024-03-13 11:12:52 -0300

    add schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,37 @@ import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { FinancialTransactionModule } from './financial-transaction/financial-transaction.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 /**
+ *  Configure o .env
+ * 
  *  npm i @nestjs/config
- *
  *
  *  ConfigModule.forRoot(),
  *  */
 
+/**
+ * Validação de schema
+ * 
+ * npm install @hapi/joi
+ * npm install --save-dev @types/hapi__joi
+ * 
+ * Para validar que uma variável de ambiente foi configurada use a Joi.required() dentro do Joi.object({}), Tudo que não for configurado ali será opicional.
+ * Você pode passar um valor default entre outros (consultar doc da lib)
+ * 
+ * 
+ * 
+ */
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      })
+    }),
     AccountsModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // tipo do banco de dados

e63070155f660a002b032c4f6e7f993e3597c434
EddieUFSM <eddie.maneki@gmail.com>
2024-03-13 18:25:51 -0300

    config e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,8 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .set('Authorization', process.env.API_KEY) // 👈 
       .expect(200)
-      .expect('Hello World!');
-  });
+      .expect({server: 'on'});
+  }); 
 });

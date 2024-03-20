b2e515713df845f134bc2b54da2437a5a6297bfb
EddieUFSM <eddie.maneki@gmail.com>
2024-03-20 18:42:33 -0300

    fix script and package.json

--- scripts/main.sh
+++ scripts/main.sh
@@ -15,14 +15,15 @@ end_docker() {
 # Função para iniciar a aplicação
 start_app() {
     echo "Iniciando a aplicação..."
-    nest start &
+    npm start &
     PID=$!
 }
 
 # Função para abrir um novo terminal e executar o script para gerar o Swagger JSON
 generate_swagger() {
     echo "Abrindo um novo terminal para gerar o Swagger JSON..."
-    gnome-terminal -- bash -c 'sleep 10; curl -o swagger.json http://localhost:3000/api-json;'
+    gnome-terminal -- bash -c 'sleep 10; curl -o swagger.json http://localhost:3000/api-json; sleep 5; kill -9 $PID; exit'
+    end_app
 }
 
 # Função para encerrar a aplicação
@@ -35,5 +36,4 @@ end_app() {
 start_docker
 start_app
 generate_swagger
-end_app
 end_docker

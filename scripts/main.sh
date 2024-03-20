61fb69742a7cf638ed26398bd1a469192fd72780
EddieUFSM <eddie.maneki@gmail.com>
2024-03-20 19:05:45 -0300

    end app after generate swagger

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,46 +3,52 @@
 # Função para iniciar o Docker
 start_docker() {
     echo "Iniciando o docker..."
-    docker compose up -d
+    docker-compose up -d
 }
 
 # Função para encerrar o Docker
 end_docker() {
     echo "Encerrando o docker..."
-    docker compose down
+    docker-compose down
 }
 
 # Função para iniciar a aplicação
 start_app() {
     echo "Iniciando a aplicação..."
     npm start &
-    PID=$!
-}
-
-# Função para abrir um novo terminal e executar o script para gerar o Swagger JSON
-generate_swagger() {
-    echo "Abrindo um novo terminal para gerar o Swagger JSON..."
-    gnome-terminal -- bash -c 'sleep 10; curl -o swagger.json http://localhost:3000/api-json; sleep 5; kill -9 $PID; exit'
+    APP_PID=$!
 }
 
 # Função para encerrar a aplicação
 end_app() {
     echo "Encerrando a aplicação..."
-    kill $PID  # Encerra o processo da aplicação
+    kill $APP_PID  # Encerra o processo da aplicação
 }
 
-check_and_end_app() {
-    if [ -f "swagger_generated" ]; then
-        end_app
+# Função para verificar se a aplicação está em execução
+is_app_running() {
+    if ps -p $APP_PID > /dev/null; then
+        return 0  # Processo está em execução
     else
-        sleep 1
-        check_and_end_app
+        return 1  # Processo não está em execução
     fi
 }
 
-# Iniciar o Docker, a aplicação, abrir um novo terminal para gerar o Swagger JSON e encerrar o Docker
+# Iniciar o Docker e a aplicação
 start_docker
 start_app
-generate_swagger
-check_and_end_app
+
+# Aguardar um pouco para garantir que a aplicação esteja em execução
+sleep 10
+
+# Gerar o Swagger JSON
+echo "Gerando o Swagger JSON..."
+curl -o swagger.json http://localhost:3000/api-json
+
+# Verificar se a aplicação está em execução e encerrar, se necessário
+if is_app_running; then
+    end_app
+fi
+
+# Encerrar o Docker
 end_docker
